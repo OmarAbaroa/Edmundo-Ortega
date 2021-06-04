332072f4858d06afd985ad0917a359a6b901f8ac
OmarAbaroa <omarac1295@gmail.com>
2021-06-04 14:01:28 -0500

    Animaciones

--- src/page.jsx
+++ src/page.jsx
@@ -3,6 +3,19 @@ import { Navbar, Nav, NavDropdown, Accordion, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { Planos } from './assets/images';
+import { motion, AnimatePresence } from "framer-motion"
+
+const Image = ({ isVisible, src }) => (
+    <AnimatePresence>
+        {
+            isVisible && (
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 0.8 }} transition={{ duration: 1.5 }} exit={{ opacity: 0 }} > 
+                    <img className = 'img-fluid' src = { src }  alt  = 'Planos' /> 
+                </motion.div>
+            )
+        }
+    </AnimatePresence>
+)
 
 class Page extends Component {
 
@@ -41,33 +54,33 @@ class Page extends Component {
                                 </div>
                                 <Accordion defaultActiveKey="0">
                                     <Accordion.Toggle eventKey="0" as = { Button } variant="link" className ='collapse-accordion-button' >
-                                        <span><FontAwesomeIcon icon = { faCircle } /></span> Proyecto arquitectónico
+                                        <span><FontAwesomeIcon className ='bullet'  icon = { faCircle } /></span> Proyecto arquitectónico
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="0">
-                                        <p className="mt-2">
+                                        <p className="mt-2 text-justify">
                                             Quisque rutrum arcu quis venenatis mattis. Mauris sem dui, tempus at neque vel, cursus venenatis nunc. Ut rutrum ultricies arcu, id gravida elit pharetra a. Nunc a ex sed urna mattis molestie. In hac habitasse platea dictumst. Vivamus a quam sit amet risus commodo tincidunt. Integer ut fringilla mi. In luctus, erat ut fermentum vestibulum, dolor nisi euismod mi, eu finibus ligula mi id nunc. Quisque id rutrum leo. Donec nec tempor dolor, vel cursus libero. Donec varius finibus libero sit amet venenatis. Donec ornare est enim, fringilla posuere velit pharetra vitae. Aliquam quis consectetur nisl. Morbi suscipit pretium dolor. Maecenas maximus dictum gravida.
                                         </p>
                                     </Accordion.Collapse>
                                     <Accordion.Toggle eventKey="1" as = { Button } variant="link" className ='collapse-accordion-button' >
-                                        <span><FontAwesomeIcon icon = { faCircle } /></span> Anteproyecto
+                                        <span><FontAwesomeIcon className ='bullet'  icon = { faCircle } /></span> Anteproyecto
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="1">
-                                        <p className="mt-2">
+                                        <p className="mt-2 text-justify">
                                             Quisque rutrum arcu quis venenatis mattis. Mauris sem dui, tempus at neque vel, cursus venenatis nunc. Ut rutrum ultricies arcu, id gravida elit pharetra a. Nunc a ex sed urna mattis molestie. In hac habitasse platea dictumst. Vivamus a quam sit amet risus commodo tincidunt. Integer ut fringilla mi. In luctus, erat ut fermentum vestibulum, dolor nisi euismod mi, eu finibus ligula mi id nunc. Quisque id rutrum leo. Donec nec tempor dolor, vel cursus libero. Donec varius finibus libero sit amet venenatis. Donec ornare est enim, fringilla posuere velit pharetra vitae. Aliquam quis consectetur nisl. Morbi suscipit pretium dolor. Maecenas maximus dictum gravida.
                                         </p>
                                     </Accordion.Collapse> 
                                     <Accordion.Toggle eventKey="2" as = { Button } variant="link" className ='collapse-accordion-button' >
-                                        <span><FontAwesomeIcon icon = { faCircle } /></span> Construcción & Remodelación
+                                        <span><FontAwesomeIcon className ='bullet'  icon = { faCircle } /></span><span>Construcción & Remodelación</span> 
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="2">
-                                        <p className="mt-2">
+                                        <p className="mt-2 text-justify">
                                             Quisque rutrum arcu quis venenatis mattis. Mauris sem dui, tempus at neque vel, cursus venenatis nunc. Ut rutrum ultricies arcu, id gravida elit pharetra a. Nunc a ex sed urna mattis molestie. In hac habitasse platea dictumst. Vivamus a quam sit amet risus commodo tincidunt. Integer ut fringilla mi. In luctus, erat ut fermentum vestibulum, dolor nisi euismod mi, eu finibus ligula mi id nunc. Quisque id rutrum leo. Donec nec tempor dolor, vel cursus libero. Donec varius finibus libero sit amet venenatis. Donec ornare est enim, fringilla posuere velit pharetra vitae. Aliquam quis consectetur nisl. Morbi suscipit pretium dolor. Maecenas maximus dictum gravida.
                                         </p>
                                     </Accordion.Collapse> 
                                 </Accordion>
                             </div>
-                            <div className="col-md-6 col-lg-4 p-md-5 p-2">
-                                <img className = 'img-fluid' src = { Planos }  alt  = 'Planos' />
+                            <div className="col-md-6 col-lg-4 p-md-5 p-2 d-flex align-items-center">
+                                <Image isVisible = {1} src = {Planos} />
                             </div>
                         </div>
                     </div>
